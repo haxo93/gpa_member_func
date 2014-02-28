963fe69ed06d09bc5c2864965294e20c73c68141
suddenrelic <ryan.coakley93@gmail.com>
2014-02-28 15:31:32 -0500

    Update student.cpp

--- student.cpp
+++ student.cpp
@@ -10,35 +10,44 @@ Student::Student()
 {
     name = "";
     credits = 0;
-    qp = 0;
+    qp = 0.0;
 }
 
 Student::Student(string tempname,int tempcredits, double tempqp)
 {
-    name = tempname;
-    if (tempcredits == 0 && tempqp == 0)
-        return;
-    if (tempcredits > 0)
-        credits = tempcredits;
-    else
-        credits = 0;
-    if (tempqp > 0)
-        qp = tempqp;
-    else
-        qp = 0;
+   name = tempname;
+   credits = tempcredits;
+   qp = tempqp;
 
 }
 
 double Student::computeGPA()
 {
-    if (credits > 0 || qp > 0)
-        return qp/credits;
-    else
+    if (credits == 0)
+    {
+        return 0.0 ;
+    }
+        else if 
+    {
+        (isalpha(credits))
+    }    
         return 0.0;
+        
+    else 
+    {
+        studentGPA = qp/credits;
+        return studentGPA;
+    }
 }
+
 
 void Student::addGrade(string tempgrade, int tempcredits)
 {
+    if (credits <0) 
+    {
+        credits == 0 
+    }
+        return 0.0;
     credits += tempcredits;
     double v = findGradeValue(tempgrade);
     qp += tempcredits * v;
